d23a93eb92accc78a3937fbac39534c68b9b228b
geekysandy11 <153822055+asg-01@users.noreply.github.com>
2026-08-15 00:32:14 +0530

    Add Google Tag Manager (GTM-NZRN6R2B)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import AudioProvider from "@/context/AudioProvider";
 import GlobalPlayer from "@/components/GlobalPlayer";
 import AIChatBot from "@/components/AIChatBot";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import Script from "next/script";
 import "./globals.css";
 
 const inter = Inter({
@@ -29,11 +30,32 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${inter.variable} ${playfair.variable} dark`}>
+    <html lang="en" className={`${inter.variable} ${playfair.variable} dark}>
+      <head>
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='++dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NZRN6R2B');`
+          }}
+        />
+      </head>
       <body className="antialiased bg-black text-slate-200">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTE-NZRN6R2B"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <AudioProvider>
           <Navbar />
-          {children}
+           {children}
           <Footer />
           <GlobalPlayer />
           <AIChatBot />
